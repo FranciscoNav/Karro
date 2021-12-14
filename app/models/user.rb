7863f2d7c85e1efcc99d99962d78dea46996398a
Francisco <fnaveira11@gmail.com>
2021-12-14 16:14:42 -0500

    fixed bug with validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,9 @@
 class User < ApplicationRecord
     validates :name, presence: :true
     validates :name, uniqueness: { case_sensitive: true }
-    validates :password_confirmation, presence: :true
 
     has_secure_password
-    
+
     has_many :expenses
     has_many :cars, through: :expenses
 end
--- client/src/components/EditForm.js
+++ client/src/components/EditForm.js
@@ -43,7 +43,7 @@ const EditForm = (props) => {
                 <br/>
                 <br/>
                 <input className="button" type="submit"/>
-                <button className='button' onClick={() => props.setEditFormFlag(false) }>Back</button>
+                <button className='button' onClick={() => props.setEditFormFlag(false)}>Back</button>
             </form>
         </div>
     )
